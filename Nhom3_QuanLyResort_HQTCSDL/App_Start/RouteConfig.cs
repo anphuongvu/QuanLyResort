9dd057aeefcea1193bd7d956603cdb11d1e568e0
anphuongvu <vuphuongan2012@gmail.com>
2022-12-16 16:10:53 +0700

    first commit

---
+++ Nhom3_QuanLyResort_HQTCSDL/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Nhom3_QuanLyResort_HQTCSDL
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "AdminPage",
+                "Admin/{controller}/{action}",
+                new { area = "Admin", controller = "Home", action = "Index" },
+                new[] { typeof(Areas.Admin.Controllers.HomeController).Namespace }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "Nhom3_QuanLyResort_HQTCSDL.Controllers" }
+            );
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
---
+++ Nhom3_QuanLyResort_HQTCSDL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nhom3_QuanLyResort_HQTCSDL.MvcApplication" Language="C#" %>
